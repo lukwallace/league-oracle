cf796a2d3e54aebc27f8e7afe7e514c48eaf3aeb
Wallace <wallace.s.luk@gmail.com>
2017-03-20 18:15:46 -0700

    Add basic endpoint

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Oracle = require('./oracle');
+
+const oracle = new Oracle();
 const app = express();
 
 
-app.get('/summoner/:region/:name', (req, res) => { res.send(req.params); });
+app.get('/summoner/:region/:name', (req, res) => { 
+  const { region, name } = req.params;
+  oracle.region = region;
+  oracle.generateMatrix(name)
+  .then(matrix => {
+    res.send(matrix);
+  });
+});
+
 app.get('*', (req, res) => { res.status(400).send(); });
 
 // Used for testing
