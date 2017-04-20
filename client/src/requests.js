b65a65f9b3b68e3a24aa0b6242cacc5fd8625b9b
Wallace <wallace.s.luk@gmail.com>
2017-04-19 23:55:06 -0700

    Minor endpoint comms

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,3 +1,19 @@
 const SERVER = process.env.SERVER || 'http://localhost:8080';
 
 
+const fetchChampionList = () => {
+  return fetch(SERVER + '/champions')
+  .then(res => res.json());
+}
+
+let fetchMatrix = (summonerName) => {
+  return fetch(SERVER + '/summoner/na/' + summonerName)
+  .then(res => res.json());
+};
+
+fetchMatrix = () => new Promise((resolve, reject) => resolve('Stubbed!'));
+
+module.exports = {
+  fetchChampionList,
+  fetchMatrix,
+};
