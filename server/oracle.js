cf197f01abf4518f9f64a044afb24835a75de026
Wallace <wallace.s.luk@gmail.com>
2017-04-26 18:27:46 -0700

    Using ES6 classes

--- server/oracle.js
+++ server/oracle.js
@@ -1,6 +1,24 @@
 const Promise = require('bluebird');
+const redis = require('redis');
+const Profile = require('./profile/profileModel.js');
+
+// const client = redis.createClient();
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+/* Some test code for redis */
+// client.on('ready', () => {
+//   console.log('Redis connection open and ready!')
+// });
+// client.on('error', (err) => {
+//   console.log('We got problems:', err);
+// });
+
+// client.set('key', 'value', redis.print);
+// client.getAsync('key').then(res => console.log(res));
+
 const { getChampionIndex, getSummonerId, getMatchRefs, getMatch } = require('./requests');
-const { parseMatch, sanitize } = require('./helpers');
+const { parseMatch } = require('./helpers');
 
 /* Singleton Object for the webserver  -- for now all data is stored locally
 Will keep track of the summoners it has served in the local memory for now. Will later be uprooted by whatever database is deemed fit. Right now considering MongoDB for matricies, and Redis for summoner id and lastMatch */
@@ -12,56 +30,79 @@ Example:
   Keep a list of users that we get match data for. Every night, update from each user's matrix with match data using the list, if user hasn't had any new matches in the last 3 days, remove from the list. Users that request a matrix will automatically be placed in the list. Users that haven't requested a matrix in the last week or so but are on the list are skipped and removed.  
 */
 
-const Oracle = function() {
-  this.summoners = {};
-  this.region = 'na';
-  this.championIndexPromise = this.loadChampionIndex();
-};
-
-/* Methods */
-Oracle.prototype.loadChampionIndex = function() {
-  return getChampionIndex(this.region)
-  .then(json => json.data);
-};
-
-
-Oracle.prototype.storeSummoner = function(name) {
-  return getSummonerId(this.region, name)
-  .then(id => this.summoners[sanitize(name)] = { id, lastMatch: undefined, matrix: {} });
-};
-
-Oracle.prototype.getData = function(name) {
-  const { region, summoners, championIndexPromise } = this;
-  const { id, lastMatch, matrix } = summoners[name];
-
-  return getMatchRefs(region, id, lastMatch)
-  .then(matchRefs => {
-    if(matchRefs.length !== 0) {
-      this.summoners[name].lastMatch = matchRefs[0].timestamp + 1;
-    }
-    const matches = [];
-    matchRefs.forEach(matchRef => {
-      const { champion, matchId } = matchRef;
-      matches.push(getMatch(region, matchId, champion));
-    })
-    return Promise.all([Promise.all(matches), championIndexPromise]);
-  })
-  .spread((matches, championIndex)  => {
-    matches.forEach(match => parseMatch(match, matrix, championIndex))
-    return matrix;
-  });
-}
-
-Oracle.prototype.generateMatrix = function(name) {
-  const { summoners } = this;
-  let summoner = summoners[name];
-
-  if(summoner === undefined) {
-    return this.storeSummoner(name)
-    .then(() => this.getData(name));
+class Oracle {
+  constructor () {
+    this.summoners = {};
+    this.region = 'na';
+    this.championIndexPromise = this.loadChampionIndex();
   }
 
-  return this.getData(name);
+  loadChampionIndex () {
+    return getChampionIndex(this.region)
+    .then(json => json.data);
+  }
+
+
+  fetchSummoner (name) {
+    const { summoners, region } = this;
+    const key = region + ':' + name;
+    if(summoners[key]) {
+      return new Promise((resolve, reject) => {
+        resolve(summoners[key]);
+      });
+    }
+
+    return getSummonerId(this.region, name)
+    .then(id => {
+      const summoner = { id, lastMatch: undefined };
+      this.summoners[key] = summoner;
+      return summoner;
+    });
+  }
+
+  fetchMatrix (name) {
+    let id;
+    return this.fetchSummoner(name)
+    .then(summoner => {
+      id = summoner.id;
+      return Profile.findOne({ id }).exec()
+    })
+    .then(profile => {
+      console.log('Searched profiles');
+      if(profile) {
+        return profile.matrix;
+      } else {
+        return this.initializeMatrix(name, id)
+      }
+    });
+  }
+
+  initializeMatrix (name, id) {
+    const matrix = {};
+    let lastMatch = -1;
+    const { region, championIndexPromise } = this;
+
+    return getMatchRefs(region, id)
+    .then(matchRefs => {
+      if(matchRefs.length !== 0) {
+        lastMatch = matchRefs[0].timestamp + 1;
+      }
+      const matches = [];
+      matchRefs.forEach(matchRef => {
+        // const { champion, matchId } = matchRef;
+        matches.push(getMatch(region, matchRef));
+      })
+      return Promise.all([Promise.all(matches), championIndexPromise]);
+    })
+    .spread((matches, championIndex)  => {
+      matches.forEach(match => parseMatch(match, matrix, championIndex))
+      const profile = new Profile({ id, name, region, lastMatch, matrix });
+      return profile.save();
+    })
+    .then(profile => {
+      return profile.matrix;
+    });
+  }
 };
 
 module.exports = Oracle;
