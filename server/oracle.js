2f68f539772436d580442a9def35ffc1c82885ec
Wallace <wallace.s.luk@gmail.com>
2017-05-06 15:24:45 -0700

    Clean up

--- server/oracle.js
+++ server/oracle.js
@@ -1,3 +1,4 @@
+/* Main file */
 const Promise = require('bluebird');
 const redis = require('redis');
 const Profile = require('./profile/profileModel.js');
@@ -6,7 +7,7 @@ const client = redis.createClient();
 Promise.promisifyAll(redis.RedisClient.prototype);
 Promise.promisifyAll(redis.Multi.prototype);
 
-/* Some test code for redis */
+/* Connect to Redis */
 client.on('ready', () => {
   console.log('Redis connection open and ready!')
 });
@@ -14,26 +15,17 @@ client.on('error', (err) => {
   console.log('We got problems:', err);
 });
 
-// client.flushall(redis.print);
-// client.hmsetAsync('na:wallace', 'key', 'value', 'lastMatch', 'asd').then(res => console.log(res));
-// client.hgetallAsync('na:wale').then(res => console.log(res));
-
 /* Riot API calls */
 const { getChampionIndex, getSummonerId, getMatchRefs, getMatch } = require('./requests');
 const { parseMatch } = require('./helpers');
 
-/* Singleton Object for the webserver  -- for now all data is stored locally
-Will keep track of the summoners it has served in the local memory for now. Will later be uprooted by whatever database is deemed fit. Right now considering MongoDB for matricies, and Redis for summoner id and lastMatch */
+/* For stubbing Redis callbacks */
+const noop = () => {};
 
-/* 
-Scheduling Updates vs. Getting them ad hoc -- for the long term, it might be better to have a system that updates the a summoner's matrix scheduled. Currently, a request for matrix data creates a Riot API request that must check if a player has any new matches. Assuming that a user may want to access matrix data multiple times between matches, there may be a lot of false positives, i.e. there will be a Riot API request on every matrix request even when there isn't any new match data to add to the matrix. Since we won't know if there are any new matches for a user until we actually check, (Riot doesn't have a webhook notifications) scheduling checks proportional to the number of times a matrix is requested seems like a good idea.
-
-Example: 
-  Keep a list of users that we get match data for. Every night, update from each user's matrix with match data using the list, if user hasn't had any new matches in the last 3 days, remove from the list. Users that request a matrix will automatically be placed in the list. Users that haven't requested a matrix in the last week or so but are on the list are skipped and removed.  
-*/
-
+/* Main Singleton */
 class Oracle {
   constructor () {
+    this._redis = client;
     this.region = 'na';
     this.championIndexPromise = this.loadChampionIndex();
   }
@@ -48,7 +40,7 @@ class Oracle {
     const key = region + ':' + name;
     let cached = false;
 
-    return client.hgetAsync(key, 'id')
+    return client.hgetAsync(key, 'id', noop)
     .then(id => {
       if(id) {
         cached = true;
@@ -91,7 +83,7 @@ class Oracle {
     })
     .spread((matches, championIndex)  => {
       matches.forEach(match => parseMatch(match, matrix, championIndex));
-      client.hset(region + ':' + name, 'lastMatch', profile.lastMatch);
+      client.hset(region + ':' + profile.name, 'lastMatch', profile.lastMatch, noop);
       return profile.save();
     })
     .then(profile => {
@@ -101,3 +93,13 @@ class Oracle {
 };
 
 module.exports = Oracle;
+
+/* 
+Topics to touch on:
+Why need Redis/Cache
+
+Scheduling Updates vs. Getting them ad hoc -- for the long term, it might be better to have a system that updates the a summoner's matrix scheduled. Currently, a request for matrix data creates a Riot API request that must check if a player has any new matches. Assuming that a user may want to access matrix data multiple times between matches, there may be a lot of false positives, i.e. there will be a Riot API request on every matrix request even when there isn't any new match data to add to the matrix. Since we won't know if there are any new matches for a user until we actually check, (Riot doesn't have a webhook notifications) scheduling checks proportional to the number of times a matrix is requested seems like a good idea.
+
+Example: 
+  Keep a list of users that we get match data for. Every night, update from each user's matrix with match data using the list, if user hasn't had any new matches in the last 3 days, remove from the list. Users that request a matrix will automatically be placed in the list. Users that haven't requested a matrix in the last week or so but are on the list are skipped and removed.  
+*/
--- server/test/oracle.js
+++ server/test/oracle.js
@@ -1,7 +1,19 @@
 const Oracle = require('../oracle');
+const redis = require('redis');
+const Profile = require('../profile/profileModel');
 
-const o = new Oracle();
-o.fetchMatrix('wallace')
+const oracle = new Oracle();
+
+console.log('Flushing Redis . . .');
+oracle._redis.flushallAsync()
+.then(() => {
+  console.log('Flushed Redis, truncating Profiles . . .')
+  return Profile.remove({})
+})
+.then(() => {
+  console.log('Profiles truncated, fetching matrix');
+  return oracle.fetchMatrix('wallace')
+})
 .then(matrix => {
   console.log(matrix);
 });
